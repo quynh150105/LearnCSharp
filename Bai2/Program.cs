0445e8d382da29d4cc23c47a37616f13013d18b8
quynh150105 <quynh1512005@gmail.com>
2025-08-30 23:42:45 +0700

    add hdt

---
+++ Bai2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Bai2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student hs = new Student();
+        hs.setName("Quynh");
+        hs.setAge(20);
+        Console.WriteLine(hs.toString());
+    }
+}
---
+++ Bai2/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    public class Student
+    {
+        private String name;
+        private int age;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public String getName()
+        {
+            return this.name;
+        }
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public String toString()
+        {
+            return "name: " + this.name + " " + "age: " + this.age;
+        }
+    }
+}
