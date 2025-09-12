85964a5c0322aa014b6f179c89d11fb703f79def
quynh150105 <quynh1512005@gmail.com>
2025-09-12 16:00:19 +0700

    add btvn xmlSchema

---
+++ LeNhuQuynh_2023600638_XmlSchema/Form1.cs
@@ -0,0 +1,10 @@
+namespace LeNhuQuynh_2023600638_XmlSchema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
