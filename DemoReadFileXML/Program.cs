595d2f62e8ff3f1d6a655c9478d260fb9d18162a
quynh150105 <quynh1512005@gmail.com>
2025-09-24 09:20:23 +0700

    add read filexml 2.0

--- DemoReadFileXML/Program.cs
+++ DemoReadFileXML/Program.cs
@@ -1,25 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more informatio
 
-using System.Xml;
+//using System.Xml;
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-Console.WriteLine("Chương trình đọc file xml");
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
+//Console.WriteLine("Chương trình đọc file xml");
 
-XmlDocument doc = new XmlDocument();
-doc.Load("XMLFile1.xml");
-XmlElement root = doc.DocumentElement;
-XmlNodeList list = root.SelectNodes("cuonsach");
-int i = 1;
-foreach(XmlNode item in list)
-{
-    Console.WriteLine("------------------");
-    Console.WriteLine("Phần tử thứ: " + i);
-    Console.WriteLine("Ten sach: " + item.SelectSingleNode("tensach").InnerText);
-    Console.WriteLine("So trang: " + item.SelectSingleNode("sotrang").InnerText);
-    Console.WriteLine("Ho ten tac gia: " + item.SelectSingleNode("tacgia/hoten").InnerText);
-    Console.WriteLine("Dien thoai tac gia: " + item.SelectSingleNode("tacgia/dienthoai").InnerText);
-    Console.WriteLine("Gia tien: " + item.SelectSingleNode("giatien").InnerText);
-    i++;
-}
-Console.ReadKey();
+//XmlDocument doc = new XmlDocument();
+//doc.Load("XMLFile1.xml");
+//XmlElement root = doc.DocumentElement;
+//XmlNodeList list = root.SelectNodes("cuonsach");
+//int i = 1;
+//foreach(XmlNode item in list)
+//{
+//    Console.WriteLine("------------------");
+//    Console.WriteLine("Phần tử thứ: " + i);
+//    Console.WriteLine("Ten sach: " + item.SelectSingleNode("tensach").InnerText);
+//    Console.WriteLine("So trang: " + item.SelectSingleNode("sotrang").InnerText);
+//    Console.WriteLine("Ho ten tac gia: " + item.SelectSingleNode("tacgia/hoten").InnerText);
+//    Console.WriteLine("Dien thoai tac gia: " + item.SelectSingleNode("tacgia/dienthoai").InnerText);
+//    Console.WriteLine("Gia tien: " + item.SelectSingleNode("giatien").InnerText);
+//    i++;
+//}
+//Console.ReadKey();
 
---
+++ DemoReadFileXML/ReadFile2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DemoReadFileXML
+{
+    internal class ReadFile2
+    {
+        static void Main(string[] args)
+        {
+           Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("chuong trinh doc file.xml 2.0");
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("XMLFile1.xml");
+            XmlElement root = doc.DocumentElement;
+            PrintNode(root);
+            Console.ReadKey();
+        }
+        static void PrintNode(XmlNode node)
+        {
+            Console.WriteLine("Type =[" + node.NodeType + "]");
+            Console.WriteLine("Name =[" + node.Name + "]");
+            Console.WriteLine("Value =[" + node.Value + "]");
+
+            XmlNodeList children = node.ChildNodes;
+
+            if(children.Count > 0)
+            {
+                Console.WriteLine("cac con cua node " + node.Name + " la: ");
+                foreach(XmlNode item in children)
+                {
+                    PrintNode(item);
+                }
+                Console.WriteLine("ket thuc node " + node.Name);
+            }
+        }
+    }
+}
