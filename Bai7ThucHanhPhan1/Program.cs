9f60d735fb91627f5ba985f64054f1881dd7df99
quynh150105 <quynh1512005@gmail.com>
2025-09-25 22:29:57 +0700

    create function themsach

--- Bai7ThucHanhPhan1/Program.cs
+++ Bai7ThucHanhPhan1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using Bai7ThucHanhPhan1;
+using System.Xml;
 
 class Program
 {
@@ -6,20 +7,30 @@ class Program
     {
         XmlDocument doc = new XmlDocument();
         XmlElement root = doc.CreateElement("thuvien");
+        CuonSach s1 = new CuonSach("j01","cong nghe thong tin","lap trinh java","500","Le Nhu quynh","0123456789","50000");
+        ThemSach(doc, root, s1);
+        CuonSach s2 = new CuonSach("C01", "cong nghe thong tin", "lap trinh C++", "500", "Le Nhu quynh", "0123456789", "50000");
+        ThemSach(doc, root, s2);
+        doc.AppendChild(root);
+        doc.Save("thuvien.xml");
+    }
+
+     static public void ThemSach(XmlDocument doc, XmlElement root, CuonSach s)
+    {
         XmlElement cuonsach = doc.CreateElement("cuonsach");
-        cuonsach.SetAttribute("masach", "j01");
-        cuonsach.SetAttribute("theloai", "cong nghe thong tin");
+        cuonsach.SetAttribute("masach", s.masach);
+        cuonsach.SetAttribute("theloai", s.theloai);
         XmlElement tensach = doc.CreateElement("tensach");
-        XmlText ttensach = doc.CreateTextNode("lap trinh java");
+        XmlText ttensach = doc.CreateTextNode(s.tensach);
         XmlElement sotrang = doc.CreateElement("sotrang");
-        XmlText tsotrang = doc.CreateTextNode("500");
-        XmlElement tacgia = doc.CreateElement("tacgiaa");
+        XmlText tsotrang = doc.CreateTextNode(s.sotrang);
+        XmlElement tacgia = doc.CreateElement("tacgia");
         XmlElement hoten = doc.CreateElement("hoten");
-        XmlText thoten = doc.CreateTextNode("LeNhuQuynh");
+        XmlText thoten = doc.CreateTextNode(s.hoten);
         XmlElement dienthoai = doc.CreateElement("dienthoai");
-        XmlText tdienthoai = doc.CreateTextNode("0123456789");
+        XmlText tdienthoai = doc.CreateTextNode(s.dienthoai);
         XmlElement giatien = doc.CreateElement("giatien");
-        XmlText tgiatien = doc.CreateTextNode("50000");
+        XmlText tgiatien = doc.CreateTextNode(s.giatien);
         tensach.AppendChild(ttensach);
         sotrang.AppendChild(tsotrang);
         hoten.AppendChild(thoten);
@@ -32,7 +43,6 @@ class Program
         cuonsach.AppendChild(giatien);
         tacgia.AppendChild(hoten);
         tacgia.AppendChild(dienthoai);
-        doc.AppendChild(root);
-        doc.Save("thuvien.xml");
     }
+
 }
