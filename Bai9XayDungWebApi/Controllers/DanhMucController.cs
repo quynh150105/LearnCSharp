422832087f613a19c57a5237e8ab5597a975b4a9
quynh150105 <quynh1512005@gmail.com>
2025-10-26 16:06:37 +0700

    add Bai9 Xay dung Web API

---
+++ Bai9XayDungWebApi/Controllers/DanhMucController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bai9XayDungWebApi.Controllers
+{
+    public class DanhMucController : ApiController
+    {
+        [HttpGet]
+        public List<DanhMuc> getAllDanhMuc() { 
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<DanhMuc> ds = context.DanhMucs.ToList();
+            
+            foreach(DanhMuc dm in ds)
+            {
+                dm.SanPhams.Clear();
+            }
+            return ds;
+        
+        }
+        [HttpGet]
+        public DanhMuc getDanhMuc(int id)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            DanhMuc dm = context.DanhMucs
+                .First(x => x.MaDanhMuc == id);
+            if(dm!=null)
+            {
+                dm.SanPhams.Clear();
+            }
+            return dm;
+        }
+    }
+}
---
+++ Bai9XayDungWebApi/Controllers/SanPhamController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bai9XayDungWebApi.Controllers
+{
+    public class SanPhamController : ApiController
+    {
+        [HttpGet]
+        public List<SanPham> getAllSanPham() {
+
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<SanPham> dsSanPham = context.SanPhams.ToList();
+            foreach(SanPham sp in dsSanPham)
+            {
+                sp.DanhMuc = null;
+            }
+            return dsSanPham;
+
+        }
+        [HttpGet]
+        public SanPham getSanPham(int id)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            SanPham sp = context.SanPhams
+                .First(x => x.Ma == id);
+            if(sp!= null)
+            {
+                sp.DanhMuc = null;
+            }
+            return sp;
+        }
+        [HttpGet]
+        public List<SanPham> getSanPhamByMaDanhMuc(int maDanhMuc)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<SanPham> ds = context.SanPhams
+                .Where(x => x.MaDanhMuc == maDanhMuc)
+                .ToList();
+            foreach (SanPham sp in ds)
+                sp.DanhMuc = null;
+            return ds;
+
+        }
+        [HttpGet]
+        public List<SanPham> getByCost(int a, int b)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<SanPham> ds = context.SanPhams
+                .Where(x => x.DonGia > a && x.DonGia <= b)
+                .ToList();
+            foreach (SanPham sp in ds)
+                sp.DanhMuc = null;
+            return ds;
+        }
+
+
+
+    }
+}
