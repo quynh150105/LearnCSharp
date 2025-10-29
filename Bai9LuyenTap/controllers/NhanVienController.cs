48a96da4b85f0dbc7bf533d06ee190e62351a086
quynh150105 <quynh1512005@gmail.com>
2025-10-29 22:57:59 +0700

    bai 9 luyen crud web api

---
+++ Bai9LuyenTap/controllers/NhanVienController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bai9LuyenTap.controllers
+{
+    public class NhanVienController : ApiController
+    {
+        [HttpGet]
+        public List<NhanVien> DanhsachNV()
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            List<NhanVien> ds = context.NhanViens.ToList();
+            return ds;
+        }
+        [HttpPost]
+        public bool ThemNhanVien([FromBody] NhanVien nv)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            context.NhanViens.InsertOnSubmit(nv);
+            context.SubmitChanges();
+            return true;
+        }
+        [HttpDelete]
+        public bool XoaNhanvien(string id)
+        {
+            try
+            {
+                CSDLTestDataContext context = new CSDLTestDataContext();
+                NhanVien nv = context.NhanViens.FirstOrDefault(x => x.MaNV == id);
+                if (nv == null) return false;
+
+                context.NhanViens.DeleteOnSubmit(nv);
+                context.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        
+        }
+
+    }
+}
