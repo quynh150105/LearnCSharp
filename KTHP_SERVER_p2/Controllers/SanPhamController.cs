61d59623f3c71465ddc7bb86d6268570b6c62066
quynh150105 <quynh1512005@gmail.com>
2025-12-29 12:01:50 +0700

    add kthp final server

---
+++ KTHP_SERVER_p2/Controllers/SanPhamController.cs
@@ -0,0 +1,86 @@
+﻿using KTHP_SERVER_p2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Common;
+
+namespace KTHP_SERVER_p2.Controllers
+{
+    public class SanPhamController : ApiController
+    {
+        CSDLTestEntities db = new CSDLTestEntities();
+
+        [HttpGet]
+        public List<SanPham> getAllSanPham()
+        {
+            return db.SanPhams.ToList();
+        }
+
+        [HttpGet]
+        public List<SanPham> getByMaSanPham(int ma)
+        {
+            return db.SanPhams.Where(x => x.MaSanPham == ma).ToList();
+        }
+
+        [HttpPost]
+        public bool createSanPham(int ma, string ten, int gia, int madm)
+        {
+            SanPham sp = db.SanPhams.FirstOrDefault(x=> x.MaSanPham == ma);
+            if(sp != null)
+            {
+                return false;
+            }
+            else
+            {
+                sp = new SanPham();
+                sp.MaSanPham = ma;
+                sp.TenSanPham = ten;
+                sp.DonGia = gia;
+                sp.MaDanhMuc = madm;
+                db.SanPhams.Add(sp);
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+        [HttpDelete]
+        public bool deleteSanPham(int ma)
+        {
+            SanPham sp = db.SanPhams.FirstOrDefault(x => x.MaSanPham == ma);
+            if (sp == null)
+            {
+                return false;
+            }
+            else
+            {
+                db.SanPhams.Remove(sp);                
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+        [HttpPatch]
+        public bool updateSanPham(int ma, string ten, int gia, int madm)
+        {
+            SanPham sp = db.SanPhams.FirstOrDefault(x => x.MaSanPham == ma);
+            if(sp == null)
+            {
+                return false;
+            }
+            else
+            {
+                sp.TenSanPham = ten;
+                sp.DonGia = gia;
+                sp.MaDanhMuc = madm;
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+
+
+    }
+}
---
+++ KTHP_SERVER_p2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KTHP_SERVER_p2.WebApiApplication" Language="C#" %>
